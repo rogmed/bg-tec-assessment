c9cbb23d83162112ebdda956e28bcf77355708e5
rogmed <rogeliomdn@gmail.com>
2026-05-09 16:11:21 +0200

    Question 3 - skeleton project for AgeCalculator

---
+++ question03-age-calculator/question3-AgeCalculator/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using question3_AgeCalculator.Models;
+using System.Diagnostics;
+
+namespace question3_AgeCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
